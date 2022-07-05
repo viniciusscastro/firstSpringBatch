618a2a45ac6646515b6fe15d92001a209d8834c7
viniciusscastro <viniciuss.castro@hotmail.com>
2022-07-05 20:23:52 -0300

    second commit

--- primeiroprojetoSpringBatch/src/main/java/com/udemy/primeiroprojetoSpringbatch/BatchConfig.java
+++ primeiroprojetoSpringBatch/src/main/java/com/udemy/primeiroprojetoSpringbatch/BatchConfig.java
@@ -1,5 +1,47 @@
 package com.udemy.primeiroprojetoSpringbatch;
 
-public class BatchConfig {
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory; 
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job imprimeOlaJob() {
+		return jobBuilderFactory
+				.get("imprimeOlaJob")
+				.start(imprimeOlaStep())
+				.build();
+	}
+
+	private Step imprimeOlaStep() {
+		// TODO Auto-generated method stub
+		return stepBuilderFactory
+				.get("imprimeOlaStep")
+				.tasklet(new Tasklet() {
+
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+							throws Exception {
+						System.out.println("Ola , mundo");
+						return RepeatStatus.FINISHED;
+					}})
+				.build();
+	}
 }
